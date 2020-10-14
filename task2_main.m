d4cd6b75b4ef4e2f6056d79d306def3a46854807
shivani26v <61066540+shivani26v@users.noreply.github.com>
2020-10-14 23:08:42 +0530

    Create task2_main.m

---
+++ task2_main.m
@@ -0,0 +1,9 @@
+% main, use this file to run the code
+
+task2_initialisations; %edit file for different cases
+
+task2_measurement_model; %plots measurements too
+
+task2_KalmanFilter; %plots true and estimates states
+
+%bye
